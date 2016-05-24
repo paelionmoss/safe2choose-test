68671a8902a96b39eda525b3436af10863ca1b4f
Phil Aelion-Moss <philip.aelionmoss@gmail.com>
2016-05-24 00:46:23 -0400

    Refactor

--- chat.js
+++ chat.js
@@ -2,129 +2,44 @@
   var url = window.location.href;
   var locale = url.split("/")[3];
   var onlineGreeting, offlineGreeting;
-
-  if(locale === "pl") {
-    window.$zopim||(function(d,s){var z=$zopim=function(c){
-    z._.push(c)},$=z.s=
-    d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
-    _.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute('charset','utf-8');
-    $.src='//v2.zopim.com/?';z.t=+new Date;$.
-    type='text/javascript';e.parentNode.insertBefore($,e)})(document,'script');
-
-    onlineGreeting = "Chat with us";
-    offlineGreeting = 'Leave us a message';
-    $zopim(function() {
-	    $zopim.livechat.setGreetings({
-	    'online': onlineGreeting,
-	    'offline': offlineGreeting
-	    });
-    });
-  } else if(locale === "es") {
-    window.$zopim||(function(d,s){var z=$zopim=function(c){
-    z._.push(c)},$=z.s=
-    d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
-    _.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute('charset','utf-8');
-    $.src='//v2.zopim.com/?';z.t=+new Date;$.
-    type='text/javascript';e.parentNode.insertBefore($,e)})(document,'script');
-
-    onlineGreeting = "Habla con nosotras";
-    offlineGreeting = 'Deja un mensaje';
-    $zopim(function() {
-	    $zopim.livechat.setGreetings({
-	    'online': onlineGreeting,
-	    'offline': offlineGreeting
-	    });
-    });
-
-  } else if(locale === "pt-pt") {
-    window.$zopim||(function(d,s){var z=$zopim=function(c){
-    z._.push(c)},$=z.s=
-    d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
-    _.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute('charset','utf-8');
-    $.src='//v2.zopim.com/?';z.t=+new Date;$.
-    type='text/javascript';e.parentNode.insertBefore($,e)})(document,'script');
-    onlineGreeting = "Chat with us";
-    offlineGreeting = 'Leave us a message';
-    $zopim(function() {
-	    $zopim.livechat.setGreetings({
-	    'online': onlineGreeting,
-	    'offline': offlineGreeting
-	    });
-    });
-  } else if(locale === "hi") {
-    window.$zopim||(function(d,s){var z=$zopim=function(c){
-    z._.push(c)},$=z.s=
-    d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
-    _.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute('charset','utf-8');
-    $.src='//v2.zopim.com/?';z.t=+new Date;$.
-    type='text/javascript';e.parentNode.insertBefore($,e)})(document,'script');
-    onlineGreeting = "Chat with us";
-    offlineGreeting = 'Leave us a message';
-    $zopim(function() {
-	    $zopim.livechat.setGreetings({
-	    'online': onlineGreeting,
-	    'offline': offlineGreeting
-	    });
-    });
-  } else if(locale === "fr") {
-    window.$zopim||(function(d,s){var z=$zopim=function(c){
-    z._.push(c)},$=z.s=
-    d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
-    _.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute('charset','utf-8');
-    $.src='//v2.zopim.com/?';z.t=+new Date;$.
-    type='text/javascript';e.parentNode.insertBefore($,e)})(document,'script');
-    onlineGreeting = "Chat with us";
-    offlineGreeting = 'Leave us a message';
-    $zopim(function() {
-	    $zopim.livechat.setGreetings({
-	    'online': onlineGreeting,
-	    'offline': offlineGreeting
-	    });
-    });
-  } else if(locale === "ja") {
-    window.$zopim||(function(d,s){var z=$zopim=function(c){
-    z._.push(c)},$=z.s=
-    d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
-    _.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute('charset','utf-8');
-    $.src='//v2.zopim.com/?';z.t=+new Date;$.
-    type='text/javascript';e.parentNode.insertBefore($,e)})(document,'script');
-    onlineGreeting = "私たちとチャット";
-    offlineGreeting = '私達にメッセージを残します';
-    $zopim(function() {
-	    $zopim.livechat.setGreetings({
-	    'online': onlineGreeting,
-	    'offline': offlineGreeting
-	    });
-    });
-  } else if(locale === "it-it") {
-    window.$zopim||(function(d,s){var z=$zopim=function(c){
-    z._.push(c)},$=z.s=
-    d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
-    _.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute('charset','utf-8');
-    $.src='//v2.zopim.com/?';z.t=+new Date;$.
-    type='text/javascript';e.parentNode.insertBefore($,e)})(document,'script');
-    onlineGreeting = "Chatta con noi";
-    offlineGreeting = 'Ci lascia un messaggio';
-    $zopim(function() {
-	    $zopim.livechat.setGreetings({
-	    'online': onlineGreeting,
-	    'offline': offlineGreeting
-	    });
-    });
-  } else {
-    window.$zopim||(function(d,s){var z=$zopim=function(c){
-    z._.push(c)},$=z.s=
-    d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
-    _.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute('charset','utf-8');
-    $.src='//v2.zopim.com/?';z.t=+new Date;$.
-    type='text/javascript';e.parentNode.insertBefore($,e)})(document,'script');
-    onlineGreeting = "Chat with us";
-    offlineGreeting = 'Leave us a message';
-    $zopim(function() {
-	    $zopim.livechat.setGreetings({
-	    'online': onlineGreeting,
-	    'offline': offlineGreeting
-	    });
-    });
+  
+  onlineGreeting = "";
+  offlineGreeting = "";
+  
+  switch (locale) {
+    case "es":
+      onlineGreeting = "Habla con nosotras";
+      offlineGreeting = "Deja un mensaje";
+      break;
+    case "ja":
+      onlineGreeting = "私たちとチャット";
+      offlineGreeting = "私達にメッセージを残します";
+      break;
+    case "it-it":
+      onlineGreeting = "Chatta con noi";
+      offlineGreeting = "Ci lascia un messaggio";
+      break;
+    // NOTE: It seems a bit weird that these languages don't have translated greetings but 
+    //       this is just refactoring so I'm leaving behavior as is.
+    case "pl":
+    case "pt-pt":
+    case "hi":
+    case "fr":
+    default:
+      onlineGreeting = "Chat with us";
+      offlineGreeting = "Leave us a message";
   }
+  
+  window.$zopim||(function(d,s){var z=$zopim=function(c){
+    z._.push(c)},$=z.s=
+    d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
+    _.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute('charset','utf-8');
+    $.src='//v2.zopim.com/?';z.t=+new Date;$.
+    type='text/javascript';e.parentNode.insertBefore($,e)})(document,'script');
+  
+  $zopim.livechat.setGreetings({
+	    'online': onlineGreeting,
+	    'offline': offlineGreeting
+	    });
+  
   </script>
