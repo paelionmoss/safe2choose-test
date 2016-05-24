458fdea9367d004a28c5700bbfaa66d9a5dbc662
Phil Aelion-Moss <philip.aelionmoss@gmail.com>
2016-05-24 00:29:10 -0400

    Add Japanese and Italian chat

--- chat.js
+++ chat.js
@@ -81,6 +81,36 @@
 	    'offline': offlineGreeting
 	    });
     });
+  } else if(locale === "ja") {
+    window.$zopim||(function(d,s){var z=$zopim=function(c){
+    z._.push(c)},$=z.s=
+    d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
+    _.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute('charset','utf-8');
+    $.src='//v2.zopim.com/?';z.t=+new Date;$.
+    type='text/javascript';e.parentNode.insertBefore($,e)})(document,'script');
+    onlineGreeting = "私たちとチャット";
+    offlineGreeting = '私達にメッセージを残します';
+    $zopim(function() {
+	    $zopim.livechat.setGreetings({
+	    'online': onlineGreeting,
+	    'offline': offlineGreeting
+	    });
+    });
+  } else if(locale === "it-it") {
+    window.$zopim||(function(d,s){var z=$zopim=function(c){
+    z._.push(c)},$=z.s=
+    d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
+    _.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute('charset','utf-8');
+    $.src='//v2.zopim.com/?';z.t=+new Date;$.
+    type='text/javascript';e.parentNode.insertBefore($,e)})(document,'script');
+    onlineGreeting = "Chatta con noi";
+    offlineGreeting = 'Ci lascia un messaggio';
+    $zopim(function() {
+	    $zopim.livechat.setGreetings({
+	    'online': onlineGreeting,
+	    'offline': offlineGreeting
+	    });
+    });
   } else {
     window.$zopim||(function(d,s){var z=$zopim=function(c){
     z._.push(c)},$=z.s=
